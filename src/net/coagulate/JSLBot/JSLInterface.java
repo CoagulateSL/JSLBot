7a3f2344d6165aae0f3b9b39e2676fb2fe17f4c0
Iain Price (LUNA) <git@predestined.net>
2018-08-13 02:11:01 +0100

    Some basic JSLInterface methods

--- src/net/coagulate/JSLBot/JSLInterface.java
+++ src/net/coagulate/JSLBot/JSLInterface.java
@@ -1,5 +1,9 @@
 package net.coagulate.JSLBot;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /** JSL Public "API" (the useful part that does all the work for you).
  *
  * @author Iain Price <git@predestined.net>
@@ -7,5 +11,33 @@ package net.coagulate.JSLBot;
 public class JSLInterface {
     private JSLBot bot;
     public JSLInterface(JSLBot bot) { this.bot=bot; }
+
+    public void instantMessage(String uuid, String message) throws IOException {
+        Map<String,String> cmd=new HashMap<>();
+        cmd.put("uuid",uuid);
+        cmd.put("message",message);
+        new CommandEvent(bot, bot.getRegional(), "im", cmd, null).submitAndWait();
+    }
+
+    public void groupInvite(String avataruuid, String groupuuid) throws IOException {
+        groupInvite(avataruuid,groupuuid,null);
+    }
     
+    public void groupInvite(String avataruuid, String groupuuid, String roleuuid) throws IOException {
+        Map<String,String> cmd=new HashMap<>();
+        cmd.put("avataruuid",avataruuid);
+        cmd.put("groupuuid",groupuuid);
+        cmd.put("roleuuid",roleuuid);
+        new CommandEvent(bot, bot.getRegional(), "groups.invite", cmd, null).submitAndWait();
+    }
+
+    public void groupEject(String avataruuid, String groupuuid) throws IOException {
+        Map<String,String> cmd=new HashMap<>();
+        cmd.put("avataruuid",avataruuid);
+        cmd.put("groupuuid",groupuuid);
+        //cmd.put("roleuuid",roleuuid);
+        new CommandEvent(bot, bot.getRegional(), "groups.eject", cmd, null).submitAndWait();
+    }
+
+
 }
