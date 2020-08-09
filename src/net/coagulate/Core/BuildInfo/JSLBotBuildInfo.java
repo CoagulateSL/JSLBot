944732a239c372f15475821420276519c66e56e6
Automated Builds (SOL) <github@predestined.net>
2020-08-09 14:52:27 +0100

    New format for build information

---
+++ src/net/coagulate/Core/BuildInfo/JSLBotBuildInfo.java
@@ -0,0 +1,10 @@
+package net.coagulate.Core.BuildInfo;
+import java.util.Date;
+/** This class is automatically generated during GitHub version rebuilds */
+public class JSLBotBuildInfo {
+	public static final int MAJORVERSION=0;
+	public static final int MINORVERSION=0;
+	public static final int BUGFIXVERSION=0;
+	public static final String COMMITID="MANUAL";
+	public static final Date BUILDDATE=new Date(0L);
+}
